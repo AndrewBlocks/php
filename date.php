d05e2811de052102e80fa25135089866b04164b8
AndrewBlocks <sterpuandrej@gmail.com>
2021-04-01 09:50:09 +0300

    393

---
+++ date.php
@@ -0,0 +1,9 @@
+<?php 
+	echo date("<br />l dS of F Y h:i:s A <br />");
+	echo date("<br />Сегодня d.m.Y <br />");
+	echo date("<br />Этот файл датирован d.m.Y <br />", filectime(__FILE__));
+
+
+	
+
+?>
